ad03eb7aa343b24bf3c1e68c0a2bfc5b27473a8b
Mauri <mauri.monzon91@gmail.com>
2024-08-23 17:08:36 -0300

    Navegacion

---
+++ src/MovieApp.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { Text } from "react-native";
+
+export const MovieApp = () => {
+  return (
+    <NavigationContainer>
+      <Text>MovieApp</Text>
+    </NavigationContainer>
+  );
+};
---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,14 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {DetailScreen} from '../screens/detail/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="DetailScreen" component={DetailScreen} />
+    </Stack.Navigator>
+  );
+};
