8f6931806b86274c4533952cdb5a9c5a443a560d
Mauri <mauri.monzon91@gmail.com>
2024-12-29 19:50:06 -0300

    Preprando el build del aab

--- android/app/src/main/java/com/movieapp/MainActivity.kt
+++ android/app/src/main/java/com/movieapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.movieapp
+package com.mauridev.infopeli
 
 import android.os.Bundle;
 
--- android/app/src/main/java/com/movieapp/MainApplication.kt
+++ android/app/src/main/java/com/movieapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.movieapp
+package com.mauridev.infopeli
 
 import android.app.Application
 import com.facebook.react.PackageList
